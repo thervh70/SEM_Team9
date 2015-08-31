55112161350902081e0097dbcb1a60492f5bff20
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2015-08-31 19:29:04 +0200

    Create empty Maven project and add Eclipse settings to .gitignore
    HelloGame and HelloGameTest are added as "default" source and test files

---
+++ src/main/java/nl/tudelft/ti2206/group9/HelloGame.java
@@ -0,0 +1,11 @@
+package nl.tudelft.ti2206.group9;
+
+public final class HelloGame {
+	
+	private HelloGame() { }
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Game!");
+	}
+	
+}
