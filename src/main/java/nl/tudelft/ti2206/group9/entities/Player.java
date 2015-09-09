2abb52823b5de3043a35f04cee60c1d49a8739e9
Maikel <maikelkerkhof@gmail.com>
2015-09-09 14:46:26 -0700

    Changed parameters to make game more fun

--- src/main/java/nl/tudelft/ti2206/group9/entities/Player.java
+++ src/main/java/nl/tudelft/ti2206/group9/entities/Player.java
@@ -17,9 +17,9 @@ public class Player extends AbstractEntity {
 	public static final double WIDTH = 0.8;
 	
 	/** Gravity. This is added to the vertical speed of the Player each tick. */
-	public static final double GRAVITY = 0.005;
+	public static final double GRAVITY = 0.025;
 	/** Jump speed. This is the initial vertical speed of the Player on jump. */
-	public static final double JUMPSPEED = 0.12;
+	public static final double JUMPSPEED = 0.4;
 	
 	/** Indicates whether the Player is alive or not. */
 	private boolean alive;
