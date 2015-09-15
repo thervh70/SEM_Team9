eb2c26a0c6dbe1ae11c607e401e8c5024adec2af
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2015-09-15 16:19:58 +0200

    #68 Create GameObservable and GameObserver
    These two should be complete, did not test this yet. Logger will be
    filled up later.
    
    < feature

---
+++ src/main/java/nl/tudelft/ti2206/group9/util/GameObservable.java
@@ -0,0 +1,45 @@
+package nl.tudelft.ti2206.group9.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nl.tudelft.ti2206.group9.util.GameObserver.Category;
+import nl.tudelft.ti2206.group9.util.GameObserver.Specific;
+
+public final class GameObservable {
+	
+	private GameObservable() { }
+	
+	private static List<GameObserver> observers = new ArrayList<GameObserver>();
+	
+	/**
+	 * Add observer to the observers list.
+	 * @param go GameObserver to add to the list.
+	 */
+	public static void addObserver(GameObserver go) {
+		observers.add(go);
+	}
+	
+	/**
+	 * Remove observer from the observers list.
+	 * @param go GameObserver to remove from the list.
+	 */
+	public static void deleteObserver(GameObserver go) {
+		observers.remove(go);
+	}
+	
+	/**
+	 * Call this method when you want to update the observers.
+	 * @param cat the Category of this update.
+	 * @param spec the Specific action of this update.
+	 * @param optionalArgs Optional arguments that come with the update
+	 * 			(e.g. lane numbers, mouse buttons, keyboard keys, ...)
+	 */
+	public static void notifyObservers(
+			Category cat, Specific spec, Object... optionalArgs) {
+		for (GameObserver go : observers) {
+			go.gameUpdate(cat, spec, optionalArgs);
+		}
+	}
+	
+}
---
+++ src/main/java/nl/tudelft/ti2206/group9/util/Logger.java
@@ -0,0 +1,10 @@
+package nl.tudelft.ti2206.group9.util;
+
+public class Logger implements GameObserver {
+
+	public void gameUpdate(Category cat, Specific spec, Object[] optionalArgs) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
