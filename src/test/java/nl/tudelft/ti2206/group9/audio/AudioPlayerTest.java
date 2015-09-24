b98021930a477857d392a456726ac7f80c841668
mdingjan <M.J.C.Dingjan@student.tudelft.nl>
2015-09-24 08:19:32 +0200

    #30 Revert; it didn't solve the Travis problem
    <bug, testing

--- src/test/java/nl/tudelft/ti2206/group9/audio/AudioPlayerTest.java
+++ src/test/java/nl/tudelft/ti2206/group9/audio/AudioPlayerTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import javafx.scene.media.MediaException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -16,20 +17,21 @@ public class AudioPlayerTest {
 	private static String testPath;
 	private AudioPlayer test;
 
-	@Test
-	public void testPlay() throws MediaException {
+	@Before
+	public void setUp() throws Exception {
 		testPath = "src/main/resources/" 
 				+ "nl/tudelft/ti2206/group9/audio/soundtrack.aiff";
 		test = new AudioPlayer(testPath);
+	}
+
+	@Test
+	public void testPlay() throws MediaException {
 		test.play();
 		assertTrue(test.isRunning());
 	}
 
 	@Test
 	public void testStop() throws MediaException {
-		testPath = "src/main/resources/" 
-				+ "nl/tudelft/ti2206/group9/audio/soundtrack.aiff";
-		test = new AudioPlayer(testPath);
 		test.play();
 		test.stop();
 		assertFalse(test.isRunning());
@@ -37,9 +39,6 @@ public class AudioPlayerTest {
 
 	@Test
 	public void testSetPath() {
-		testPath = "src/main/resources/" 
-				+ "nl/tudelft/ti2206/group9/audio/soundtrack.aiff";
-		test = new AudioPlayer(testPath);
 		String testDifferentPath = "src/main/resources/" 
 				+ "nl/tudelft/ti2206/group9/audio/test.aiff";
 		test.setPath(testDifferentPath);
