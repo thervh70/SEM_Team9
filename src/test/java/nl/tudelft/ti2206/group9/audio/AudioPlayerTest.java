1109b6c31e78397fc32079b8e97c7a1dafd1c2d7
mdingjan <M.J.C.Dingjan@student.tudelft.nl>
2015-09-24 08:08:34 +0200

    #30 Change in AudioPlayerTest
    <bug,testing

--- src/test/java/nl/tudelft/ti2206/group9/audio/AudioPlayerTest.java
+++ src/test/java/nl/tudelft/ti2206/group9/audio/AudioPlayerTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import javafx.scene.media.MediaException;
 
-import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -17,21 +16,20 @@ public class AudioPlayerTest {
 	private static String testPath;
 	private AudioPlayer test;
 
-	@Before
-	public void setUp() throws Exception {
+	@Test
+	public void testPlay() throws MediaException {
 		testPath = "src/main/resources/" 
 				+ "nl/tudelft/ti2206/group9/audio/soundtrack.aiff";
 		test = new AudioPlayer(testPath);
-	}
-
-	@Test
-	public void testPlay() throws MediaException {
 		test.play();
 		assertTrue(test.isRunning());
 	}
 
 	@Test
 	public void testStop() throws MediaException {
+		testPath = "src/main/resources/" 
+				+ "nl/tudelft/ti2206/group9/audio/soundtrack.aiff";
+		test = new AudioPlayer(testPath);
 		test.play();
 		test.stop();
 		assertFalse(test.isRunning());
@@ -39,6 +37,9 @@ public class AudioPlayerTest {
 
 	@Test
 	public void testSetPath() {
+		testPath = "src/main/resources/" 
+				+ "nl/tudelft/ti2206/group9/audio/soundtrack.aiff";
+		test = new AudioPlayer(testPath);
 		String testDifferentPath = "src/main/resources/" 
 				+ "nl/tudelft/ti2206/group9/audio/test.aiff";
 		test.setPath(testDifferentPath);
