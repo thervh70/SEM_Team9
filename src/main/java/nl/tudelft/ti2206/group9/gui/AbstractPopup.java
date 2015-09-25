3178d914040033ee1139abf6a4751b6d9aa311ed
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2015-09-25 15:00:13 +0200

    #92 Processed review comments
    <layout,naming

--- src/main/java/nl/tudelft/ti2206/group9/gui/AbstractPopup.java
+++ src/main/java/nl/tudelft/ti2206/group9/gui/AbstractPopup.java
@@ -18,29 +18,29 @@ import javafx.stage.Popup;
 @SuppressWarnings("restriction")
 public abstract class AbstractPopup extends Popup {
 
-	/** Width of the PopUpMenu. */
-	public static final double WIDTH = 260;
-	/** Height of the PopUpMenu. */
-	public static final double HEIGHT = 320;
-	/** Size of the HBox. */
-	protected static final double HBOX_SIZE = 10;
-	/** Size of the VBox. */
-	protected static final double VBOX_SIZE = 50;
+    /** Width of the PopUpMenu. */
+    public static final double WIDTH = 260;
+    /** Height of the PopUpMenu. */
+    public static final double HEIGHT = 320;
+    /** Size of the HBox. */
+    protected static final double HBOX_SPACING = 10;
+    /** Size of the VBox. */
+    protected static final double VBOX_SPACING = 50;
 
-	/** The left button in the Popup. */
-	private final Button left;
-	/** The right button in the Popup. */
-	private final Button right;
+    /** The left button in the Popup. */
+    private final Button left;
+    /** The right button in the Popup. */
+    private final Button right;
 
-	/**
-	 * Constructs a new Popup with default {@link #WIDTH} and {@link #HEIGHT}.
-	 * @param leftButton Button that should be shown in the bottom-left.
-	 * @param rightButton Button that should be shown in the bottom-right.
-	 */
-	public AbstractPopup(final Button leftButton, final Button rightButton) {
-		super();
-		left = leftButton;
-		right = rightButton;
+    /**
+     * Constructs a new Popup with default {@link #WIDTH} and {@link #HEIGHT}.
+     * @param leftButton Button that should be shown in the bottom-left.
+     * @param rightButton Button that should be shown in the bottom-right.
+     */
+    public AbstractPopup(final Button leftButton, final Button rightButton) {
+        super();
+        left = leftButton;
+        right = rightButton;
 
         setWidth(WIDTH);
         setHeight(HEIGHT);
@@ -52,37 +52,37 @@ public abstract class AbstractPopup extends Popup {
         Style.setPopupButtonStyle(left);
         Style.setPopupButtonStyle(right);
 
-        final HBox hbox = new HBox(HBOX_SIZE, left, right);
+        final HBox hbox = new HBox(HBOX_SPACING, left, right);
         hbox.setAlignment(Pos.CENTER);
 
         final Node[] content = createContent();
         final Node[] content2 = Arrays.copyOf(content, content.length + 1);
         content2[content.length] = hbox;
-        final VBox vbox = new VBox(VBOX_SIZE, content2);
+        final VBox vbox = new VBox(VBOX_SPACING, content2);
         vbox.setAlignment(Pos.CENTER);
 
         getContent().addAll(rect, vbox);
-	}
+    }
 
-	/**
-	 * This method returns the content that should be added to the VBox of the
-	 * Popup. Each next node will be placed below the previous.
-	 * @return A list of Nodes to be added to the VBox of the Popup.
-	 */
-	public abstract Node[] createContent();
+    /**
+     * This method returns the content that should be added to the VBox of the
+     * Popup. Each next node will be placed below the previous.
+     * @return A list of Nodes to be added to the VBox of the Popup.
+     */
+    public abstract Node[] createContent();
 
-	/**
-	 * @return the left Button
-	 */
-	public Button getLeftButton() {
-		return left;
-	}
+    /**
+     * @return the left Button
+     */
+    public Button getLeftButton() {
+        return left;
+    }
 
-	/**
-	 * @return the right Button
-	 */
-	public Button getRightButton() {
-		return right;
-	}
+    /**
+     * @return the right Button
+     */
+    public Button getRightButton() {
+        return right;
+    }
 
 }
