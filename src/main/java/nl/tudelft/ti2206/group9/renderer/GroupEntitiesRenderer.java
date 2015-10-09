1a00622728c2635b65ec495510fa4e1d7a4222cf
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2015-10-09 11:46:56 +0200

    #40 Optimize Player overlay
    <feature,user-interface

--- src/main/java/nl/tudelft/ti2206/group9/renderer/GroupEntitiesRenderer.java
+++ src/main/java/nl/tudelft/ti2206/group9/renderer/GroupEntitiesRenderer.java
@@ -1,8 +1,7 @@
 package nl.tudelft.ti2206.group9.renderer;
 
-import javafx.scene.DepthTest;
 import javafx.scene.Node;
-import javafx.scene.Group;
+import javafx.scene.shape.Box;
 import nl.tudelft.ti2206.group9.entities.AbstractEntity;
 import nl.tudelft.ti2206.group9.level.State;
 import nl.tudelft.ti2206.group9.util.ObservableLinkedList.Listener;
@@ -19,18 +18,18 @@ public class GroupEntitiesRenderer extends AbstractGroupRenderer
 	public GroupEntitiesRenderer() {
 		super();
 		State.getTrack().addEntitiesListener(this);
-		setDepthTest(DepthTest.ENABLE);
 
-		final Group overlays = new Group();
-		getChildren().add(overlays);
+		final Box playerOverlay = new Box();
 
 		for (final AbstractEntity e : State.getTrack().getEntities()) {
 			final AbstractBoxRenderer<?> r = e.createRenderer();
 			getChildren().add(r);
 			if (r instanceof PlayerRenderer) {
-				((PlayerRenderer) r).setOverlays(overlays);
+				((PlayerRenderer) r).setOverlay(playerOverlay);
 			}
 		}
+
+		getChildren().add(playerOverlay);
 	}
 
 	@Override
@@ -48,17 +47,18 @@ public class GroupEntitiesRenderer extends AbstractGroupRenderer
 	public void update(final Type type, final Object item, final int index) {
 		final AbstractEntity entity = (AbstractEntity) item;
 		final AbstractBoxRenderer<?> renderer = entity.createRenderer();
+		final int lastIndex = getChildren().size() - 1;
 		switch (type) {
-		case ADD_FIRST:    getChildren().add(1, renderer); break;
-		case ADD_LAST:     getChildren().add(renderer); break;
+		case ADD_FIRST:    getChildren().add(0, renderer); break;
+		case ADD_LAST:     getChildren().add(lastIndex, renderer); break;
 							// index + 1, because overlays is at index 0.
-		case REMOVE_INDEX: getChildren().remove(index + 1); break;
+		case REMOVE_INDEX: getChildren().remove(index); break;
 		case REMOVE:
 			// It is not possible to call remove(entity) on the children of this
 			// BoxRenderer. Track.children contains Entities,
 			// this.children contains BoxRenderers.
 			final int indexOf = State.getTrack().getEntities().indexOf(entity);
-			getChildren().remove(indexOf + 1);
+			getChildren().remove(indexOf);
 			break;
 		default: break;
 		}
--- src/main/java/nl/tudelft/ti2206/group9/renderer/PlayerRenderer.java
+++ src/main/java/nl/tudelft/ti2206/group9/renderer/PlayerRenderer.java
@@ -1,7 +1,5 @@
 package nl.tudelft.ti2206.group9.renderer;
 
-import javafx.application.Platform;
-import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
@@ -20,19 +18,18 @@ public class PlayerRenderer extends AbstractBoxRenderer<Player> {
 	/** Thanks to this constant, the hue rotates once every second. */
 	private static final double HUE_PER_TICK = 360 / InternalTicker.FPS;
 	/** Size of the invulnerability overlay, relative to 1. */
-	private static final double INVUL_SCALE = 1.1;
+	private static final double OVERLAY_SCALE = 1.001;
+	/** The alpha value (opacity) of the invulnerability overlay. */
+	private static final double OVERLAY_ALPHA = 0.05;
+	/** The brightness of the invulnerability overlay. */
+	private static final double OVERLAY_BRIGHT = 0.4;
 
 	/** Material for the invulnerability overlay. */
 	private final PhongMaterial invulMat = new PhongMaterial();
 	/** Box for the invulnerability overlay. */
-	private final Box invulBox = new Box();
+	private Box invulBox = new Box();
 	/** Current hue value. */
 	private double hue;
-	/** Whether the player has overlay box. */
-	private boolean hasBox;
-
-	/** The overlays group used in {@link GroupEntitiesRenderer}. */
-	private Group overlays;
 
 	/**
 	 * Default constructor.
@@ -54,21 +51,10 @@ public class PlayerRenderer extends AbstractBoxRenderer<Player> {
 		updatePosition();
 		hue += HUE_PER_TICK;
 		if (PowerupInvulnerable.isActive()) {
-			if (!hasBox) {
-				Platform.runLater(() -> {
-					overlays.getChildren().add(invulBox);
-					hasBox = true;
-				});
-			}
-			invulMat.setDiffuseColor(Color.hsb(hue, 1, 1, 1.0 / 2.0));
+			invulMat.setDiffuseColor(
+					Color.hsb(hue, 1, OVERLAY_BRIGHT, OVERLAY_ALPHA));
 		} else {
-			if (hasBox) {
-				Platform.runLater(() -> {
-					overlays.getChildren().remove(invulBox);
-					hasBox = false;
-				});
-			}
-			invulMat.setDiffuseColor(Color.hsb(hue, 1, 1, 0.0));
+			invulMat.setDiffuseColor(Color.hsb(hue, 0, 0, 0));
 		}
 	}
 
@@ -77,18 +63,19 @@ public class PlayerRenderer extends AbstractBoxRenderer<Player> {
 		invulBox.setTranslateX(getTranslateX());
 		invulBox.setTranslateY(getTranslateY());
 		invulBox.setTranslateZ(getTranslateZ());
-		invulBox.setWidth(getWidth() * INVUL_SCALE);
-		invulBox.setHeight(getHeight() * INVUL_SCALE);
-		invulBox.setDepth(getDepth() * INVUL_SCALE);
+		invulBox.setWidth(getWidth() * OVERLAY_SCALE);
+		invulBox.setHeight(getHeight() * OVERLAY_SCALE);
+		invulBox.setDepth(getDepth() * OVERLAY_SCALE);
 	}
 
 	/**
 	 * Package visibility because this only has to be done in
 	 * {@link GroupEntitiesRenderer}.
-	 * @param overlayGroup the overlays group to set
+	 * @param overlayBox the overlay group to set
 	 */
-	void setOverlays(final Group overlayGroup) {
-		overlays = overlayGroup;
+	void setOverlay(final Box overlayBox) {
+		invulBox = overlayBox;
+		invulBox.setMaterial(invulMat);
 	}
 
 }
