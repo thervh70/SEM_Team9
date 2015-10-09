94d86837d64a2bf461360eadfebac88d85b87d92
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2015-10-09 10:23:33 +0200

    #138 Add support for different font sizes
    <review,bug

--- src/main/java/nl/tudelft/ti2206/group9/gui/Style.java
+++ src/main/java/nl/tudelft/ti2206/group9/gui/Style.java
@@ -1,31 +1,33 @@
 package nl.tudelft.ti2206.group9.gui;
 
+import static nl.tudelft.ti2206.group9.ShaftEscape.OBSERVABLE;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import nl.tudelft.ti2206.group9.ShaftEscape;
 import nl.tudelft.ti2206.group9.util.GameObserver.Category;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
-import static nl.tudelft.ti2206.group9.ShaftEscape.OBSERVABLE;
-import static nl.tudelft.ti2206.group9.util.GameObserver.Error;
+import nl.tudelft.ti2206.group9.util.GameObserver.Error;
 
 /**
  * Class containing the styling for the GUI.
@@ -70,7 +72,7 @@ public final class Style {
     private static final int POPUP_TEXT = 11;
 
     /** Font used to render everything. */
-    private static Font globalFont;
+    private static Map<Integer, Font> globalFont = new ConcurrentHashMap<>();
 
     /** Hide public constructor. */
     private Style() { }
@@ -207,20 +209,20 @@ public final class Style {
      * @return returns the font.
      */
     public static Font getFont(final int size) {
-        if (globalFont != null) {
-            return globalFont;
+        if (globalFont.get(size) != null) {
+            return globalFont.get(size);
         }
         try {
-            globalFont = Font.loadFont(new FileInputStream(new
+            globalFont.put(size, Font.loadFont(new FileInputStream(new
                     File("src/main/resources/nl/tudelft/"
-                            + "ti2206/group9/gui/8bit.ttf")), size);
+                            + "ti2206/group9/gui/8bit.ttf")), size));
         } catch (FileNotFoundException e) {
             OBSERVABLE.notify(Category.ERROR, Error.IOEXCEPTION,
                     "Style.getFont(int)",
                     e.getMessage() + " - Default globalFont used");
-            globalFont = Font.font("Roboto", FontWeight.BOLD, size);
+            globalFont.put(size, Font.font("Roboto", FontWeight.BOLD, size));
         }
-        return globalFont;
+        return globalFont.get(size);
     }
 
 }
