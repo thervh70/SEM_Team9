49f9429c95147187e95a75e4ac668e5bea109c33
mdingjan <M.J.C.Dingjan@student.tudelft.nl>
2015-10-13 18:40:59 +0200

    #124 (TDD) added a SoundEffectPlayerTest.
    This is just a template for the final tests!
    
    <testing

---
+++ src/test/java/nl/tudelft/ti2206/group9/audio/SoundEffectPlayerTest.java
@@ -0,0 +1,33 @@
+package nl.tudelft.ti2206.group9.audio;
+
+
+/**
+ * - Template of what the tests will look like! -
+ * AudioPlayerTest tests the AudioPlayer class.
+ * @author Mitchell
+ */
+public class SoundEffectPlayerTest {
+
+/*
+    //@Test
+    public void testStop() {
+        final String testPath = "src/main/resources/"
+                + "nl/tudelft/ti2206/group9/audio/soundtrack.aiff";
+        final SoundEffectPlayer test = new SoundEffectPlayer(testPath);
+        test.play();
+        test.stop();
+        //assertFalse(test.isRunning());
+    }
+
+    //@Test
+    public void testSetPath() {
+        final String testPath = "src/main/resources/"
+                + "nl/tudelft/ti2206/group9/audio/soundtrack.aiff";
+        final SoundEffectPlayer test = new SoundEffectPlayer(testPath);
+        final String testDifferentPath = "src/main/resources/"
+                + "nl/tudelft/ti2206/group9/audio/coin.wav";
+        test.setPath(testDifferentPath);
+        //assertTrue(testDifferentPath.equals(test.getPath()));
+    }
+*/
+}
