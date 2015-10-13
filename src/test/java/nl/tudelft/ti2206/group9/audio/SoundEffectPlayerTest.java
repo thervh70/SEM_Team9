a05c24434a72d04ff3213a2b1ba1931f702192d3
mdingjan <M.J.C.Dingjan@student.tudelft.nl>
2015-10-13 19:30:04 +0200

    #124 Finalized the SoundEffectPlayerTest.
    <testing

--- src/test/java/nl/tudelft/ti2206/group9/audio/SoundEffectPlayerTest.java
+++ src/test/java/nl/tudelft/ti2206/group9/audio/SoundEffectPlayerTest.java
@@ -1,25 +1,27 @@
 package nl.tudelft.ti2206.group9.audio;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 /**
- * - Template of what the tests will look like! -
- * AudioPlayerTest tests the AudioPlayer class.
+ * SoundEffectPlayerTest tests the SoundEffectPlayer class.
  * @author Mitchell
  */
 public class SoundEffectPlayerTest {
 
-/*
-    //@Test
+    @Test
     public void testStop() {
         final String testPath = "src/main/resources/"
                 + "nl/tudelft/ti2206/group9/audio/soundtrack.aiff";
         final SoundEffectPlayer test = new SoundEffectPlayer(testPath);
         test.play();
         test.stop();
-        //assertFalse(test.isRunning());
+        assertFalse(test.isRunning());
     }
 
-    //@Test
+    @Test
     public void testSetPath() {
         final String testPath = "src/main/resources/"
                 + "nl/tudelft/ti2206/group9/audio/soundtrack.aiff";
@@ -27,7 +29,7 @@ public class SoundEffectPlayerTest {
         final String testDifferentPath = "src/main/resources/"
                 + "nl/tudelft/ti2206/group9/audio/coin.wav";
         test.setPath(testDifferentPath);
-        //assertTrue(testDifferentPath.equals(test.getPath()));
+        assertTrue(testDifferentPath.equals(test.getPath()));
     }
-*/
+
 }
