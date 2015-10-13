70b1dd98a60b6c62fd6704df05a859e1954aa87c
mdingjan <M.J.C.Dingjan@student.tudelft.nl>
2015-10-13 20:14:56 +0200

    #124 (TDD) Added a SoundtrackPlayerTest.
    <testing

---
+++ src/test/java/nl/tudelft/ti2206/group9/audio/SoundtrackPlayerTest.java
@@ -0,0 +1,41 @@
+package nl.tudelft.ti2206.group9.audio;
+
+
+/**
+ * This is just a template for the final test!
+ * SoundtrackPlayerTest tests the SoundtrackPlayer class.
+ * @author Mitchell
+ */
+public class SoundtrackPlayerTest {
+
+	/** Path to a test audio file. */
+	private final String testPath = "src/main/resources/"
+            + "nl/tudelft/ti2206/group9/audio/soundtrack.aiff";
+
+/*
+    @Test
+    public void testStop() {
+        final SoundtrackPlayer test = new SoundtrackPlayer(testPath);
+        test.play();
+        test.stop();
+        assertFalse(test.isRunning());
+    }
+
+    @Test
+    public void testPause() {
+        final SoundtrackPlayer test = new SoundtrackPlayer(testPath);
+        test.play();
+        test.pause();
+        assertFalse(test.isRunning());
+    }
+
+    @Test
+    public void testSetPath() {
+        final SoundtrackPlayer test = new SoundtrackPlayer(testPath);
+        final String testDifferentPath = "src/main/resources/"
+                + "nl/tudelft/ti2206/group9/audio/coin.wav";
+        test.setPath(testDifferentPath);
+        assertTrue(testDifferentPath.equals(test.getPath()));
+    }
+*/
+}
