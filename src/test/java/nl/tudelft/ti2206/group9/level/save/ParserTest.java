b87d62b3b48240201b5730e2e39c631de52b624f
Maikel <maikelkerkhof@gmail.com>
2015-10-15 13:41:18 +0200

    Tests now working
    <testing

--- src/test/java/nl/tudelft/ti2206/group9/level/save/ParserTest.java
+++ src/test/java/nl/tudelft/ti2206/group9/level/save/ParserTest.java
@@ -1,6 +1,8 @@
 package nl.tudelft.ti2206.group9.level.save;
 
 import static org.junit.Assert.assertEquals;
+
+import nl.tudelft.ti2206.group9.gui.Style;
 import nl.tudelft.ti2206.group9.level.State;
 
 import org.junit.Test;
@@ -16,13 +18,30 @@ public class ParserTest {
 
     @Test
     public void testParser() {
+        Style.loadSkins();
         Parser.loadGame(fileFolder + "saveGameParserTest.json");
 
         final int expectedCoins = 42;
-        final int expectedHigh = 2560;
+        final int expectedHigh = 3560;
+        final boolean andy = false;
+        Style.getAndy().setSkinUnlocked(andy);
+        final boolean boy = false;
+        Style.getBoy().setSkinUnlocked(boy);
+        final boolean captain = false;
+        Style.getCaptain().setSkinUnlocked(captain);
+        final boolean iron = true;
+        Style.getIronMan().setSkinUnlocked(iron);
+        final boolean plank = true;
+        Style.getPlank().setSkinUnlocked(plank);
+
         assertEquals("Mathias", State.getPlayerName());
         assertEquals(expectedCoins, State.getCoins());
         assertEquals(expectedHigh, State.getHighscore());
         assertEquals(true, State.isSoundEnabled());
+        assertEquals(andy, Style.getAndy().getSkinUnlocked());
+        assertEquals(boy, Style.getBoy().getSkinUnlocked());
+        assertEquals(captain, Style.getCaptain().getSkinUnlocked());
+        assertEquals(iron, Style.getIronMan().getSkinUnlocked());
+        assertEquals(plank, Style.getPlank().getSkinUnlocked());
     }
 }
--- src/test/java/nl/tudelft/ti2206/group9/level/save/WriterTest.java
+++ src/test/java/nl/tudelft/ti2206/group9/level/save/WriterTest.java
@@ -1,6 +1,9 @@
 package nl.tudelft.ti2206.group9.level.save;
 
 import static org.junit.Assert.assertEquals;
+
+import nl.tudelft.ti2206.group9.gui.Style;
+import nl.tudelft.ti2206.group9.gui.skin.*;
 import nl.tudelft.ti2206.group9.level.State;
 
 import org.junit.Test;
@@ -16,14 +19,26 @@ public final class WriterTest {
 
     @Test
     public void testSaveGame() {
+        Style.loadSkins();
+
         final String playerName = "Henk";
         State.setPlayerName(playerName);
         final int coins = 45;
         State.setCoins(coins);
-        final int score = 34567;
+        final int score = 3560;
         State.setHighscore(score);
-        final boolean soundEnabled = false;
+        final boolean soundEnabled = true;
         State.setSoundEnabled(soundEnabled);
+        final boolean andy = false;
+        Style.getAndy().setSkinUnlocked(andy);
+        final boolean boy = false;
+        Style.getBoy().setSkinUnlocked(boy);
+        final boolean captain = false;
+        Style.getCaptain().setSkinUnlocked(captain);
+        final boolean iron = true;
+        Style.getIronMan().setSkinUnlocked(iron);
+        final boolean plank = true;
+        Style.getPlank().setSkinUnlocked(plank);
 
         Writer.saveGame(fileFolder + "saveGameWriterTest.json");
         Parser.loadGame(fileFolder + "saveGameWriterTest.json");
@@ -32,5 +47,11 @@ public final class WriterTest {
         assertEquals(coins, State.getCoins());
         assertEquals(score, State.getHighscore());
         assertEquals(soundEnabled, State.isSoundEnabled());
+        assertEquals(andy, Style.getAndy().getSkinUnlocked());
+        assertEquals(boy, Style.getBoy().getSkinUnlocked());
+        assertEquals(captain, Style.getCaptain().getSkinUnlocked());
+        assertEquals(iron, Style.getIronMan().getSkinUnlocked());
+        assertEquals(plank, Style.getPlank().getSkinUnlocked());
+
     }
 }
